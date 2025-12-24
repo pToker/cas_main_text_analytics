0013715ebf231707315c7b14fdbf84320d16087e
pToker <2834939+pToker@users.noreply.github.com>
2025-12-24 15:19:11 +0100

    remove conflict of having my own logging.py file which interferes with python logging but I still have an issue with uvicorn in main.py so I have to call it externally instead of python app/main.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
-import os
 from dotenv import load_dotenv
+import logging
+import os
 
 load_dotenv("ENV")
 
@@ -9,3 +10,15 @@ DATABASE_URL = f"postgresql+asyncpg://mailapp:{DATABASE_PWD}@localhost/mail_clas
 DATABASE_URL_ALEMBIC = f"postgresql+psycopg2://mailapp:{DATABASE_PWD}@localhost/mail_classifier"
 
 GMAIL_SCOPES = ["https://www.googleapis.com/auth/gmail.modify"]
+
+
+def setup_logging():
+    level_name = os.getenv("LOG_LEVEL", "WARNING").upper()
+    level = getattr(logging, level_name, logging.INFO)
+
+    logging.basicConfig(
+        level=level,
+        format="%(asctime)s %(levelname)s %(name)s %(message)s",
+        filename="app.log",
+        filemode="a",
+    )
--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI, BackgroundTasks
 from app.gmail.sync import sync_gmail
 from app.routers.sync import router as sync_router
-from app.logging import setup_logging
+from app.config import setup_logging
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 setup_logging()
 
@@ -14,3 +18,9 @@ app.include_router(sync_router)
 def run_sync(background_tasks: BackgroundTasks):
     background_tasks.add_task(sync_gmail)
     return {"status": "sync started"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host=os.getenv("HOST", "127.0.0.1"), port=int(os.getenv("PORT", 8000)), log_level=os.getenv("LOG_LEVEL", "info"))
